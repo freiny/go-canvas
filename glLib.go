63ff66db2e6b897c8f797473c0f7ef281d8bd1fe
name <freiny50@gmail.com>
2016-04-13 23:07:28 -0500

    Move OpenGL Setup code into its own file

---
+++ glLib.go
@@ -0,0 +1,124 @@
+package gowindow
+
+import (
+	"fmt"
+	"image"
+	"strings"
+
+	"github.com/go-gl/gl/v4.1-core/gl"
+)
+
+func newProgram(vertexShaderSource, fragmentShaderSource string) (uint32, error) {
+
+	vertexShader, err := compileShader(vertexShaderSource, gl.VERTEX_SHADER)
+	if err != nil {
+		return 0, err
+	}
+
+	fragmentShader, err := compileShader(fragmentShaderSource, gl.FRAGMENT_SHADER)
+	if err != nil {
+		return 0, err
+	}
+
+	program := gl.CreateProgram()
+
+	gl.AttachShader(program, vertexShader)
+	gl.AttachShader(program, fragmentShader)
+	gl.LinkProgram(program)
+
+	var status int32
+	gl.GetProgramiv(program, gl.LINK_STATUS, &status)
+	if status == gl.FALSE {
+		var logLength int32
+		gl.GetProgramiv(program, gl.INFO_LOG_LENGTH, &logLength)
+
+		log := strings.Repeat("\x00", int(logLength+1))
+		gl.GetProgramInfoLog(program, logLength, nil, gl.Str(log))
+
+		return 0, fmt.Errorf("failed to link program: %v", log)
+	}
+
+	gl.DeleteShader(vertexShader)
+	gl.DeleteShader(fragmentShader)
+
+	return program, nil
+}
+
+func compileShader(source string, shaderType uint32) (uint32, error) {
+
+	shader := gl.CreateShader(shaderType)
+
+	csources, free := gl.Strs(source)
+	gl.ShaderSource(shader, 1, csources, nil)
+	free()
+	gl.CompileShader(shader)
+
+	var status int32
+	gl.GetShaderiv(shader, gl.COMPILE_STATUS, &status)
+	if status == gl.FALSE {
+		var logLength int32
+		gl.GetShaderiv(shader, gl.INFO_LOG_LENGTH, &logLength)
+
+		log := strings.Repeat("\x00", int(logLength+1))
+		gl.GetShaderInfoLog(shader, logLength, nil, gl.Str(log))
+
+		return 0, fmt.Errorf("failed to compile %v: %v", source, log)
+	}
+
+	return shader, nil
+}
+
+func newTexture(rgba *image.RGBA) (uint32, error) {
+
+	var texture uint32
+	gl.GenTextures(1, &texture)
+	gl.ActiveTexture(gl.TEXTURE0)
+	gl.BindTexture(gl.TEXTURE_2D, texture)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
+	gl.TexImage2D(
+		gl.TEXTURE_2D,
+		0,
+		gl.RGBA,
+		int32(rgba.Rect.Size().X),
+		int32(rgba.Rect.Size().Y),
+		0,
+		gl.RGBA,
+		gl.UNSIGNED_BYTE,
+		gl.Ptr(rgba.Pix))
+
+	return texture, nil
+}
+
+var screenVertices = []float32{
+	//  X, Y,
+	-1.0, -1.0,
+	1.0, -1.0,
+	-1.0, 1.0,
+	1.0, -1.0,
+	1.0, 1.0,
+	-1.0, 1.0,
+}
+
+var vertexShader = `
+#version 330
+in vec2 vert;
+in vec2 vertTexCoord;
+out vec2 fragTexCoord;
+void main() {
+fragTexCoord = vertTexCoord;
+gl_Position = vec4((vert.x - 0.5) * 2.0, (vert.y - 0.5) * 2.0, 0, 1.0);
+}
+` + "\x00"
+
+var fragmentShader = `
+#version 330
+uniform sampler2D tex;
+in vec2 fragTexCoord;
+out vec4 outputColor;
+void main() {
+    outputColor = texture(tex, fragTexCoord);
+}
+` + "\x00"
