52f208e5e9a579e8e3a2f261de107df544efb5a2
name <freiny50@gmail.com>
2016-04-19 19:32:26 -0500

    Move library specific code to lib.go

--- lib.go
+++ lib.go
@@ -1,15 +1,51 @@
 package gowindow
 
-import "time"
+import (
+	"image"
+	"time"
 
-var debug = false
+	"github.com/go-gl/glfw/v3.1/glfw"
+)
+
+type cbRender func() *image.RGBA
+type cbCursorMove func(float64, float64)
+type cbKey func(w *Window, k Key, scancode int, action Action, mods ModifierKey)
+type cbFPS func(fps int)
+
+// Callbacks holds the callbacks defined in the User Application  ran in the library
+type Callbacks struct {
+	Render     cbRender
+	CursorMove cbCursorMove
+	Key        cbKey
+	FPS        cbFPS
+}
+
+// WinConfig holds global data (e.g. window dimensions, cursor location)
+type WinConfig struct {
+	W int
+	H int
+	X int
+	Y int
+}
+
+// Window wraps glfw.Window
+type Window struct {
+	glfw.Window
+}
+
+// Key replaces glfw.Key
+type Key int
+
+// Action replaces glfw.Action
+type Action int
+
+// ModifierKey replaces glfw.ModifierKey
+type ModifierKey int
 
 // SetDebug turn debugging mode on or off
 func SetDebug() {
 	debug = true
 }
-
-var frameToggle = false
 
 // Toggle alternates between returning true and false each frame
 func Toggle() bool {
